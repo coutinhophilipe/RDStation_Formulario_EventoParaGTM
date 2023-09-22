ce5abc30d579d59f696c32417169335b5700d7ae
Philipe Coutinho <140984753+coutinhophilipe@users.noreply.github.com>
2023-09-22 14:13:38 -0300

    Create RDStation_ValidacaoForm.js

---
+++ RDStation_ValidacaoForm.js
@@ -0,0 +1,20 @@
+
+// Criando a variável validar e informando que a tag form (html) é o valor dela
+let validar = document.querySelector('form');
+
+// Na acrescentar, na variável validar que o evento "submit" aciona a função validação
+validar.addEventListener('submit',validacao);
+
+function validacao (event) {
+
+    //Valida se o formulário foi enviado
+    if (this.checkValidity()) {
+    
+        //Envia as variáveis (event e enviou) para o GTM
+        window.dataLayer.push({
+            'event': 'FormularioEnviado',
+            'enviou': 'sim'
+        });
+
+    }
+};
